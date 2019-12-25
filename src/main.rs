594a59a9a5965fb3aab14326ee7678f0fbfdbea6
Moritz Moeller <virtualritz@gmail.com>
2019-12-25 17:40:33 +0100

    Minor fixes & support for exif v0.4.x.

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,13 @@
 #[macro_use]
 extern crate error_chain;
 
+extern crate chrono;
 extern crate clap;
 extern crate exif;
+extern crate shellexpand;
 extern crate walkdir;
 
+use chrono::NaiveTime;
 use clap::{App, Arg, ArgMatches};
 use exif::DateTime;
 use exif::Tag;
@@ -50,17 +53,7 @@ fn run() -> Result<()> {
     let args = App::new("mvimg")
         .version("0.1.0")
         .author("Moritz Moeller <virtualritz@protonmail.com>")
-        .about("Moves images into a folder hierarchy based on EXIF tags")
-        .arg(
-            Arg::with_name("SOURCE")
-                .required(true)
-                .help("Where to search for images"),
-        )
-        .arg(
-            Arg::with_name("DESTINATION")
-                .required(true)
-                .help("Where to move the images"),
-        )
+        .about("Moves images into a folder hierarchy based on EXIF DateTime tags")
         .arg(
             Arg::with_name("verbose")
                 .short("v")
@@ -105,34 +98,68 @@ fn run() -> Result<()> {
             Arg::with_name("cleanup")
                 .short("c")
                 .long("cleanup")
-                .help("Clean up removing empty directories (incl. hidden files)"),
+                .help("Remove empty directories (including hidden files)"),
+        )
+       .arg(
+            Arg::with_name("day_wrap")
+                .long("day-wrap")
+                .value_name("H[H][:M[M]]")
+                .takes_value(true)
+                .help("The time at which the date wraps to the next day (default: 00:00 aka midnight)"),
+        )
+        .arg(
+            Arg::with_name("SOURCE")
+                .required(true)
+                .help("Where to search for images"),
+        )
+        .arg(
+            Arg::with_name("DESTINATION")
+                .required(true)
+                .help("Where to move the images"),
         )
         .get_matches();
 
     let source = args.value_of("SOURCE").unwrap_or(".");
-    println!("The source folder is '{}'", source);
+
+    let time_offset =
+        NaiveTime::parse_from_str(args.value_of("day_wrap").unwrap_or("0:0"), "%H:%M").chain_err(
+            || {
+                format!(
+                    "Option --day-wrap {} is formatted incorrectly.",
+                    args.value_of("day_wrap").unwrap()
+                )
+            },
+        )?;
+
+    //println!("{}", time_offset.hour());
+    //println!("{}", time_offset.minute());
 
     for entry in WalkDir::new(source)
+        .contents_first(true)
+        .max_depth({
+            if args.is_present("recurse") {
+                std::usize::MAX
+            } else {
+                1
+            }
+        })
         .follow_links(args.is_present("dereference_symlinks"))
+        .sort_by(|a, b| a.file_name().cmp(b.file_name()))
         .into_iter()
-        .filter_entry(|e| is_image(e) || (args.is_present("recurse") && e.file_type().is_dir()))
+        .filter_entry(|e| e.file_type().is_dir() || has_image_extension(e))
     {
         let dir_entry = entry.unwrap();
 
         if !dir_entry.file_type().is_dir() {
-            println!("{}", dir_entry.path().display());
-
-            match move_image(
+            if let Err(e) = move_image(
                 dir_entry.path(),
                 Path::new(args.value_of("DESTINATION").unwrap_or(".")),
+                time_offset,
                 &args,
             ) {
-                Err(e) => {
-                    if args.is_present("halt") {
-                        return Err(e);
-                    }
+                if args.is_present("halt") {
+                    return Err(e);
                 }
-                Ok(_) => (),
             }
         }
     }
@@ -140,7 +167,12 @@ fn run() -> Result<()> {
     Ok(())
 }
 
-fn move_image(source_file: &Path, dest_dir: &Path, args: &ArgMatches) -> Result<()> {
+fn move_image(
+    source_file: &Path,
+    dest_dir: &Path,
+    _time_offset: NaiveTime,
+    args: &ArgMatches,
+) -> Result<()> {
     let source_file_handle = std::fs::File::open(source_file)
         .chain_err(|| format!("Unable to open '{}'.", source_file.display()))?;
 
@@ -152,20 +184,35 @@ fn move_image(source_file: &Path, dest_dir: &Path, args: &ArgMatches) -> Result<
             )
         })?;
 
-    let time_stamp = exif::DateTime::from(
-        meta_data
-            .get_field(Tag::DateTimeOriginal, false)
-            .and_then(|f| match f.value {
-                Value::Ascii(ref vec) if !vec.is_empty() => DateTime::from_ascii(vec[0]).ok(),
-                _ => None,
-            })
-            .unwrap(),
-    );
+    let time_stamp = meta_data
+        .get_field(Tag::DateTimeOriginal, exif::In::PRIMARY)
+        .and_then(|f| match f.value {
+            Value::Ascii(ref vec) if !vec.is_empty() => DateTime::from_ascii(&vec[0]).ok(),
+            _ => None,
+        })
+        //.unwrap(),
+        .chain_err(|| format!("Timestamp metadata missing in '{}'.", source_file.display()))?;
 
     let path = dest_dir
         .join(format!("{}", time_stamp.year))
         .join(format!("{:02}", time_stamp.month))
         .join(format!("{:02}", time_stamp.day));
+
+    /* + {
+            if time_stamp.hour + time_offset.hour() + {
+                if time_stamp.minute + time_offset.minute() > 59 {
+                    1
+                } else {
+                    0
+                }
+            } > 23
+            {
+                1
+            } else {
+                0
+            }
+        }
+    ));*/
 
     // Create the destiantion
     if !path.exists() {
@@ -182,51 +229,13 @@ fn move_image(source_file: &Path, dest_dir: &Path, args: &ArgMatches) -> Result<
             .to_lowercase(),
     );
 
-    println!("source file: '{}'", source_file.display());
-    println!("destination file: '{}'", dest_file.display());
-
-    if source_file == dest_file {
-        if args.is_present("verbose") {
-            println!("'{}' is already in place, skipping.", source_file.display());
-        }
-    //bail!();
-    } else if dest_file.exists() {
-        if source_file
-            .metadata()
-            .chain_err(|| format!("Unable to read size of '{}'.", source_file.display()))?
-            .len()
-            == std::fs::File::open(&dest_file)
-                .chain_err(|| format!("Unable to open '{}'.", source_file.display()))?
-                .metadata()
-                .chain_err(|| format!("Unable to read size of '{}'.", source_file.display()))?
-                .len()
-        {
-            if args.is_present("remove_source_if_target_exists") {
-                remove_file(source_file, args.is_present("use_rip"))?;
-            } else {
-                if args.is_present("verbose") {
-                    println!(
-                        "'{}' xists and has different size; not moving '{}'.",
-                        dest_file.display(),
-                        source_file.display()
-                    );
-                }
-            }
-        }
-    } else {
-        // Move file
-        fs::rename(source_file, dest_file).chain_err(|| {
-            format!(
-                "Unable to move '{}' to '{}'.",
-                source_file.display(),
-                dest_dir.display()
-            )
-        })?
-    }
+    move_file(source_file, &dest_file, args)?;
 
     // Move possible sidecar files
     //let source_xmp_file = source_file
     //    .with_extension(source_file.extension()?.to_str()?.to_owned() + ".xmp");
+
+    //TODO: support uppercase extension XMP files.
 
     let source_xmp_file = PathBuf::from({
         let mut tmp = source_file.as_os_str().to_owned();
@@ -234,71 +243,15 @@ fn move_image(source_file: &Path, dest_dir: &Path, args: &ArgMatches) -> Result<
         tmp
     });
 
-    //let dest_xmp_file = Path::new( dest_file.join(".xmp") );
+    if source_xmp_file.exists() {
+        let dest_xmp_file = PathBuf::from({
+            let mut tmp = dest_file.as_os_str().to_owned();
+            tmp.push(".xmp");
+            tmp
+        });
 
-    println!("source XMP file: '{}'", source_xmp_file.display());
-    //println!("destination XMP file: '{}'", dest_xmp_file.display());
+        move_file(&source_xmp_file, &dest_xmp_file, args)?;
+    }
 
     Ok(())
 }
-
-/*if None != metaData and len( metaData ):
-        try:
-            timeStamp = metaData[ 'Exif.Photo.DateTimeOriginal' ].value
-        except:
-            try:
-                timeStamp = metaData[ 'Exif.Image.DateTime' ].value
-            except:
-                try:
-                    timeStamp = metaData[ 'Exif.Image.DateTimeDigitized' ].value
-                except:
-                    timeStamp = datetime.datetime.fromtimestamp( os.path.getmtime( sourceFile ) )
-                    #print( sourceFile + " " + str( timeStamp ) )
-                    #print( 'No metadata found that could be used to move "%s".' % sourceFile )
-                    #continue
-
-        if type( timeStamp ) == str:
-            return
-
-        path = os.path.join( destination, str( timeStamp.year ), '%02d' % timeStamp.month, '%02d' % timeStamp.day )
-
-        if not os.path.exists( path ):
-            os.makedirs( path )
-
-        destinationFile = os.path.join( path, fileName.lower() )
-
-        if sourceFile == destinationFile:
-            print( '"%s" is already in place, skipping.' % sourceFile )
-        elif os.path.exists( destinationFile ):
-            if os.path.getsize( sourceFile ) == os.path.getsize( sourceFile ):
-                print( '"%s" is a duplicate of "%s", deleting.' % ( sourceFile, destinationFile ) )
-                os.remove( sourceFile )
-            else:
-                print( '"%s" exists and is different, not moving %s.' % ( destinationFile, sourceFile ) )
-        else:
-            if verbose:
-                print( sourceFile + "\t-> " + destinationFile )
-            try:
-                shutil.move( sourceFile, destinationFile )
-            except:
-                print( 'Could not move "%s".' % sourceFile )
-
-        sourceFileSideCar = sourceFile + '.xmp'
-        destinationFileSideCar = destinationFile + '.xmp'
-
-        if os.path.exists( sourceFileSideCar ):
-            if sourceFileSideCar == destinationFileSideCar:
-                print( '"%s" is already in place, skipping.' % sourceFileSideCar )
-            elif os.path.exists( destinationFileSideCar ):
-                if os.path.getsize( sourceFileSideCar ) == os.path.getsize( destinationFileSideCar ):
-                    print( '"%s" is a duplicate of "%s", deleting.' % ( sourceFileSideCar, destinationFileSideCar ) )
-                    os.remove( sourceFileSideCar )
-                else:
-                    print( '"%s" exists and is different, not moving %s.' % ( destinationFileSideCar, sourceFileSideCar ) )
-            else:
-                print( sourceFileSideCar + "\t-> " + destinationFileSideCar )
-                try:
-                    shutil.move( sourceFileSideCar, destinationFileSideCar )
-                except:
-                    print( 'Could not move "%s".' % sourceFile )
-*/
--- src/util.rs
+++ src/util.rs
@@ -3,7 +3,7 @@ use std::fs;
 use std::process::Command;
 
 #[allow(dead_code)]
-const EXTENSIONS: &'static [&'static str] = &[
+const EXTENSIONS: &[&str] = &[
     // RAW file extensions
     "3fr", "ari", "arw", "bay", "cap", "cr2", "cr3", "crw", "data", "dcr", "dcs", "dng", "drf",
     "eip", "erf", "fff", "gpr", "iiq", "k25", "kdc", "mdc", "mef", "mos", "mrw", "nef", "nrw",
@@ -11,18 +11,20 @@ const EXTENSIONS: &'static [&'static str] = &[
     "srw", "x3f", // other image files
     "fpx", "gif", "j2k", "jfif", "jif", "jp2", "jpeg", "jpg", "jpx", "pcd", "psd", "tif", "tiff",
     // movie file formats
-    "264", "3g2", "3gp", "amv", "asf", "avi", "cine", "drc", "f4a", "f4b", "f4p", "f4v", "flv", "flv",
-    "gifv", "m2ts", "m2v", "m4p", "m4v", "mkv", "mng", "mp4", "mpeg", "mpg", "mts", "mxf", "nsv",
-    "ogg", "qt", "roq", "svi", "vob", "wmv", "yuv",
+    "264", "3g2", "3gp", "amv", "asf", "avi", "cine", "drc", "f4a", "f4b", "f4p", "f4v", "flv",
+    "flv", "gifv", "m2ts", "m2v", "m4p", "m4v", "mkv", "mng", "mp4", "mpeg", "mpg", "mts", "mxf",
+    "nsv", "ogg", "qt", "roq", "svi", "vob", "wmv", "yuv",
 ];
 
-fn is_image(entry: &walkdir::DirEntry) -> bool {
+fn has_image_extension(entry: &walkdir::DirEntry) -> bool {
     entry
         .file_name()
         .to_str()
         .map(|s| {
             for ext in EXTENSIONS {
-                if s.ends_with((String::from(".") + ext).as_str()) {
+                if s.to_lowercase()
+                    .ends_with((String::from(".") + ext).as_str())
+                {
                     return true;
                 }
             }
@@ -33,7 +35,7 @@ fn is_image(entry: &walkdir::DirEntry) -> bool {
 
 fn is_program_in_path(program: &str) -> bool {
     if let Ok(path) = env::var("PATH") {
-        for p in path.split(":") {
+        for p in path.split(':') {
             let p_str = format!("{}/{}", p, program);
             if fs::metadata(p_str).is_ok() {
                 return true;
@@ -44,16 +46,64 @@ fn is_program_in_path(program: &str) -> bool {
 }
 
 fn remove_file(file: &Path, use_rip: bool) -> Result<()> {
-
-    if use_rip && is_program_in_path( "rip" ) {
+    // TODO: cache this?
+    if use_rip && is_program_in_path("rip") {
         println!("Using rip to remove file safely");
         Command::new("rip")
-            .arg( file.as_os_str() )
+            .arg(file.as_os_str())
             .output()
-            .expect(format!("Failed to execute external 'rip' command to remove '{}'.", file.display()).as_str());
-    } 
-    else {
-        fs::remove_file( file ).chain_err(|| format!("Failed to remove '{}'.", file.display()))?;
+            .unwrap_or_else(|_| {
+                panic!(
+                    "Failed to execute external 'rip' command to remove {}.",
+                    file.display()
+                )
+            });
+    } else {
+        fs::remove_file(file).chain_err(|| format!("Failed to remove {}.", file.display()))?;
+    }
+
+    Ok(())
+}
+
+fn move_file(source_file: &Path, dest_file: &Path, args: &ArgMatches) -> Result<()> {
+    if source_file == dest_file {
+        if args.is_present("verbose") {
+            println!("{} is already in place, skipping.", source_file.display());
+        }
+    //bail!();
+    } else if dest_file.exists() {
+        if source_file
+            .metadata()
+            .chain_err(|| format!("Unable to read size of '{}'.", source_file.display()))?
+            .len()
+            == std::fs::File::open(&dest_file)
+                .chain_err(|| format!("Unable to open '{}'.", source_file.display()))?
+                .metadata()
+                .chain_err(|| format!("Unable to read size of '{}'.", source_file.display()))?
+                .len()
+        {
+            if args.is_present("remove_source_if_target_exists") {
+                remove_file(source_file, args.is_present("use_rip"))?;
+            } else if args.is_present("verbose") {
+                println!(
+                    "{} exists and has different size; not moving {}.",
+                    dest_file.display(),
+                    source_file.display()
+                );
+            }
+        }
+    } else {
+        // Move file
+        if args.is_present("verbose") {
+            println!("{} ➔ {}", source_file.display(), dest_file.display());
+        }
+        fs::rename(source_file, dest_file).chain_err(|| {
+            format!(
+                "Unable to move {} to {}.",
+                source_file.display(),
+                dest_file.display()
+            )
+        })?
     }
 
     Ok(())
